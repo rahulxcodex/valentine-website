0c5a0b487557573a25dc4571e635125dffcd9728
rahulxcodex <rahulsahygupta24@gmail.com>
2025-02-13 23:27:14 +0530

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -13,19 +13,24 @@ try {
     $mail->isSMTP();
     $mail->Host       = 'smtp.gmail.com';                       //gmail SMTP server set to send through
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'user@example.com';                     //SMTP username (your gmail account)
-    $mail->Password   = 'secret';                               //SMTP password (your gmail password or app password)
+    $mail->Username   = 'rahulsah.gupta.r24@gmail.com
+';                     //SMTP username (your gmail account)
+    $mail->Password   = 'binntere';                               //SMTP password (your gmail password or app password)
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->Port       = 465;
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');         //Set the sender of the message (your email address)
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient (your bae's email address)
+    $mail->setFrom('rahulsah.gupta.r24@gmail.com
+', 'Mailer');         //Set the sender of the message (your email address)
+    $mail->addAddress('rahulsah.gupta.24@gmail.com
+', 'Joe User');     //Add a recipient (your bae's email address)
 
     //Content
     $mail->isHTML(true);
     $mail->Subject = 'I Love You';
-    $mail->Body    = 'Happy Valentine Day My Love. I love you so much. You are the best thing that has ever happened to me ❤';
+    $mail->Body    = 'Hey Ayushii, ik you have been wondering whats the use of this functionality but surpirise I can do this too and yes Ayushiii
+     Happy Valentines Day. Thik hai baki din gussa kar lena. Wanted to hug you so tightly and I miss you. Do read my letter and yes I am always for you.
+     Lots of Love and be Happy. Your Smile is precious. Thank you Bye bye.❤';
 
     $mail->send();
     echo 'Message has been sent';
